14a7d8bb6a1d0a6f9e6258bcfebbb90d7a77262d
favela <amirosalehi@gmail.com>
2024-03-07 22:30:59 -0500

    f

---
+++ day11/blackjack.py
@@ -0,0 +1,62 @@
+import random
+
+
+
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+def cardpick_user():
+    return cards[random.randint(0,12)]
+
+
+# def cardpick_pc():
+#     pc_card=[]
+#     pc_card.append(cards[random.randint(0,12)])
+#     return pc_card
+
+wanna_play = input("do you wanna play ? type y/n")
+
+if wanna_play =="y":
+    p=[cardpick_user(),cardpick_user()]
+    sum_p = 0
+    for l in p:
+        sum_p += l
+    print(f"your cards : {p}, current score : {sum_p}")
+    c = [cardpick_user()]
+    pc_sum=c[0]
+    game=True
+    print(f"computer's first card : {c}")
+    while sum_p<21 and game==True:
+        hit_stay= input("Type 'y' to get another card and 'n' to stay on this hand")
+        if hit_stay == "y":
+            p.append(cardpick_user())
+            sum_p = 0
+            for l in p:
+                sum_p += l
+            print(f"your cards : {p}, current score : {sum_p}")
+        else:
+            c.append(cardpick_user())
+            game=False
+            pc_sum=0
+            for l in c:
+                pc_sum+=l
+            while pc_sum<17:
+                c.append(cardpick_user())
+                pc_sum += c[len(c)-1]
+            print(f"your cards : {p}, current score : {sum_p}")
+    
+    if sum_p>21:
+        print("you lose.")
+        print(f"pc= {c}")
+        print(f"you = {p}")
+    elif sum_p>pc_sum:
+        print("you won.")
+        print(f"pc= {c}")
+        print(f"you = {p}")
+    elif pc_sum>sum_p and pc_sum<21:
+        print("you loose.")
+        print(f"pc= {c}")
+        print(f"you = {p}")
+    elif pc_sum>21:
+        print("you won.")
+        print(f"pc= {c}")
+        print(f"you = {p}")
